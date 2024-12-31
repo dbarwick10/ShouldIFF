a6f215b439bc8e7ca99bcfd176341b2d6ac242ed
dbarwick10 <dbarwick10@users.noreply.github.com>
2024-12-31 13:42:00 -0500

    Delete shouldiff directory

--- shouldiff/client/js/components/config/constraints.js
+++
@@ -1,4 +0,0 @@
-export const FETCH_INTERVAL_MS = 1000; // Regular polling interval
-export const RETRY_INTERVAL_MS = 60000; // Longer interval for retrying when server is down
-export const LOCAL_TESTING = true;
-export const USE_CUMULATIVE_STATS = true;
